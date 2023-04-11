252a94e456f47decfbb0f7ff2c0d19b7e5677216
Bonkesam <bonkesam9@gmail.com>
2023-04-11 06:05:27 +0300

    My functional Navbar of my airbnb clone project

---
+++ app/types/index.ts
@@ -0,0 +1,11 @@
+import { User } from "@prisma/client";
+
+export type SafeUser = Omit<
+User,
+"createdAt" | "updatedAt" | "emailverified"
+
+> & {
+    createdAt: string;
+    updatedAt: string;
+    emailVerified: string | null;
+};
