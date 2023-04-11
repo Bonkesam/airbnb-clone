8fcdc30c9ca9cc4e956d110407b81f1f6680a406
Bonkesam <bonkesam9@gmail.com>
2023-04-11 23:52:26 +0300

    Adds the Info step in the rent Modal'

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -20,7 +20,7 @@ interface MapProps {
 }
 
 
-const Map: React.FC<MapProps> = () => {
+const Map: React.FC<MapProps> = ({center = [51, -0.09]}) => {
   return (
     <MapContainer
         center={center as L.LatLngExpression || [51, -0.09]}
@@ -33,6 +33,12 @@ const Map: React.FC<MapProps> = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {center && (
+            <Marker
+                position={center as L.LatLngExpression}
+            
+            />
+        )}
     </MapContainer>
   )
 }
