295fbda29e7ac3ef68807708806001ba4195e831
Bonkesam <bonkesam9@gmail.com>
2023-04-20 16:04:13 +0300

    Fixes the css error in main page

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ const Home = async ({ searchParams } :HomeProps) => {
       <Container>
         <div className="
         pt-24
-        grig
+        grid
         grid-cols-1
         sm:grid-cols-2
         md:grid-cols-3
