5037aab6dd5993420ff23c8766e295fffdd43381
Bonkesam <bonkesam9@gmail.com>
2023-04-20 16:18:49 +0300

    Fixes the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,6 @@ const Home = async ({ searchParams } :HomeProps) => {
         <div className="
         pt-24
         grid
-        grid-cols-1
         sm:grid-cols-2
         md:grid-cols-3
         lg:grid-cols-4
